fc0935ef56a2d5a5be1389c9443f2d912097f62d
keshew <artyom.korotckov2017@yandex.ru>
2025-07-29 14:40:01 +0500

    Initial Commit

---
+++ MobileAppBallon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileAppBallon
+//
+//  Created by Артём Коротков on 29.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileAppBallon/MobileAppBallonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppBallonApp.swift
+//  MobileAppBallon
+//
+//  Created by Артём Коротков on 29.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppBallonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
